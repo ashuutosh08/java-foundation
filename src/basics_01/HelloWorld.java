64e80efc7e8c35f0b6b49d8a14f04618818bafd8
Shobhit Shukla <sshobhit2410@gmail.com>
2026-04-02 20:05:08 +0530

    Initial pull

--- src/basics_01/HelloWorld.java
+++ src/basics_01/HelloWorld.java
@@ -2,7 +2,8 @@ package basics_01;
 
 class HelloWorld {
     public static void main(String [] args){
-        System.out.print(" Hello World ");
+        System.out.println(" Hello World ");
+        System.out.print("Hello Shobhit ");
 
     }
 }
